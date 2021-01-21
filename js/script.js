ac11b59b349ed2507574013d1c5906fa75031165
Alessandro Liggieri <smmb@live.it>
2021-01-21 15:48:53 +0100

    Icone doppie

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 const icons = [
-  {name: 'cat', prefisso: 'fa-cat', type: 'animal', family: 'fas'},
-  {name: 'horse', prefisso: '', type: 'animal', family: 'fas'},
-  {name: 'dog', prefisso: '', type: 'animal', family: 'fas'},
-  {name: 'dragon', prefisso: '', type: 'animal', family: 'fas'},
-  {name: 'lemon', prefisso: '', type: 'fruit', family: 'fas'},
-  {name: 'apple', prefisso: '', type: 'fruit', family: 'fas'},
-  {name: 'user-ninja', prefisso: '', type: 'person', family: 'fas'},
-  {name: 'user-graduate', prefisso: '', type: 'person', family: 'fas'},
+  {name: 'cat', prefisso: 'fa', type: 'animal', family: 'fas'},
+  {name: 'horse', prefisso: 'fa', type: 'animal', family: 'fas'},
+  {name: 'dog', prefisso: 'fa', type: 'animal', family: 'fas'},
+  {name: 'dragon', prefisso: 'fa', type: 'animal', family: 'fas'},
+  {name: 'lemon', prefisso: 'fa', type: 'fruit', family: 'fas'},
+  {name: 'apple', prefisso: 'fa', type: 'fruit', family: 'fas'},
+  {name: 'user-ninja', prefisso: 'fa', type: 'human', family: 'fas'},
+  {name: 'user-graduate', prefisso: 'fa', type: 'human', family: 'fas'},
 ]
 
 const appContainer = document.getElementById('container');
 
 icons.forEach((element) => {
 
-  const {name, prefisso} = element
+  const {name, prefisso, family} = element
   appContainer.innerHTML += `
   <div>
-    <i class="fas fa-cat"</i>
+    <i class="${family} ${prefisso}-${name}"/>
   </div>
   `
-
 });
